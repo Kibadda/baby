82f51964bbb8b07283c24e58b0338e792184b1db
Michael Strobel <mstrobel97@gmail.com>
2023-07-31 22:16:19 +0200

    🥟 someone forgot to resolve this merge conflict

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,20 +1,14 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-<<<<<<< HEAD
         @vite(['resources/css/app.css'])
-=======
->>>>>>> 12b4da0 (🛶 add WishlistItem index/create/edit components)
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <title>{{ $title ?? 'Page Title' }}</title>
     </head>
-<<<<<<< HEAD
     <body id="front">
-=======
     <body>
->>>>>>> 12b4da0 (🛶 add WishlistItem index/create/edit components)
         {{ $slot }}
     </body>
 </html>
