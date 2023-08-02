912e235de5ac2e9f98f9259f0bb7adf86bd7e514
Michael Strobel <mstrobel97@gmail.com>
2023-08-02 21:57:16 +0200

    style(admin): add admin styles and navbar

--- resources/views/components/layouts/admin.blade.php
+++ resources/views/components/layouts/admin.blade.php
@@ -1,12 +1,24 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        @vite(['resources/sass/app.scss'])
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <title>Admin {{ $title ?? 'Page Title' }}</title>
     </head>
-    <body>
-        {{ $slot }}
+    <body id="admin">
+        <nav>
+            <ul>
+                @foreach(['wishlist'] as $name)
+                    <li class="{{ Str::contains(Route::currentRouteName(), 'admin.' . $name) ? 'active' : '' }}">
+                        <a href='{{ route("admin.$name.index") }}'>{{ _(Str::ucfirst($name)) }}</a>
+                    </li>
+                @endforeach
+            </ul>
+        </nav>
+        <main>
+            {{ $slot }}
+        </main>
     </body>
 </html>
